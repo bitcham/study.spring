f5a1a5eecb9c402e0534464f6e913398f45d9999
Chambit Oh <chambit.oh@gmail.com>
2025-04-21 21:47:17 +0300

    exception

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,44 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * Transaction - @Transactional AOP
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+      bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById( fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update( fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update( toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member Member) {
+        if(Member.getMemberId().equals("ex")){
+            throw new IllegalStateException("Error occurred during transfer");
+        }
+    }
+
+
+}
