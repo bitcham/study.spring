db97c1387cc8ab1c5407c3bad8a42581061dd424
Chambit Oh <chambit.oh@gmail.com>
2025-05-02 16:02:20 +0900

    .

--- springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -27,6 +27,7 @@ public class MemberService {
         log.info("== logRepository end ==");
     }
 
+    @Transactional
     public void joinV2(String username){
         Member member = new Member(username);
         Log logMessage = new Log(username);
